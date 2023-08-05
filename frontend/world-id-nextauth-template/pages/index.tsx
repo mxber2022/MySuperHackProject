0c109fe642e550e735ef9ccf68cdae4747cfc6c9
mxber2022 <mxber2022@gmail.com>
2023-08-05 17:34:03 +0200

    IDKit integration

--- frontend/world-id-nextauth-template/pages/index.tsx
+++ frontend/world-id-nextauth-template/pages/index.tsx
@@ -1,9 +1,28 @@
 import Layout from "../components/layout"
+import { CredentialType, IDKitWidget } from "@worldcoin/idkit";
 
 export default function IndexPage() {
+
+  async function onSuccess() {
+
+  }
+  async function handleVerify() {
+    
+  }
+
   return (
     <Layout>
       <h1>Hello Human</h1>
+      <IDKitWidget
+        app_id={`$process.env.WLD_CLIENT_ID`}
+        action="vote_1" // this is your action name from the Developer Portal
+        onSuccess={onSuccess} // callback when the modal is closed
+        handleVerify={handleVerify} // optional callback when the proof is received
+        credential_types={[CredentialType.Orb, CredentialType.Phone]} // optional, defaults to ['orb']
+        enableTelemetry // optional, defaults to false
+      >
+        {({ open }) => <button onClick={open}>Verify with World ID</button>}
+      </IDKitWidget>
     </Layout>
   )
 }
